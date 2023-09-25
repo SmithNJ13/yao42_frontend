86e2f4a0faf1fe4a73899e6a2d9aa58d10ccc0d0
smithnj13 <SmithNJ13@hotmail.com>
2023-09-25 18:11:00 +0100

    please for the love of god just do your thing

--- src/Components/export.js
+++
@@ -1,2 +0,0 @@
-export {default as CarouselComponent} from "./Carousel"
-export {default as RecipeCard} from "./RecipeCard"
--- src/Pages/SeasonalPage/index.jsx
+++ src/Pages/SeasonalPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, useParams } from 'react-router-dom'
-import {CarouselComponent, RecipeCard} from "../../Components/export"
+import {CarouselComponent, RecipeCard} from "../../Components/"
 
 import "./style.css"
 
