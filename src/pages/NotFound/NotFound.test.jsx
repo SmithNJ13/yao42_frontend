c5f800fb6cda6f84d0ac2f28888f3a51dcd0ea21
Balram Singh <balram_singh08@hotmail.com>
2023-09-25 14:40:49 +0100

    done testing for navbar and notFound page

---
+++ src/pages/NotFound/NotFound.test.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import { screen, render, cleanup } from '@testing-library/react'
+import { userEvent} from '@testing-library/user-event'
+import { MemoryRouter} from 'react-router-dom'
+import * as matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers)
+
+import NotFound from '.'
+
+
+describe("NotFound page", () => {
+
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+            <NotFound/>
+            </MemoryRouter>
+        )
+    });
+
+    afterEach(() => {
+        cleanup();
+    })
+
+    it("displays h1 with specified text", () => {
+        const h1 = screen.getByRole('heading', {level:1})
+        expect(h1).toBeInTheDocument()
+    })
+
+    it("displays h2 with specified text", () => {
+        const h2 = screen.getByRole('heading', {level:2})
+        expect(h2).toBeInTheDocument() 
+    })
+
+    it("Contains a link to home", () => {
+        const link = screen.getByRole('link')
+        expect(link).toBeInTheDocument()
+    })
+
+    it("Navigates back to home after clicking the home link", () => {
+        const link = screen.getByRole('link')
+        userEvent.click(link)
+        expect(window.location.pathname).toBe('/')
+    })
+})
