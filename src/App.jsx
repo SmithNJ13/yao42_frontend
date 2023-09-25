5da0bcad2a8b6e5c13b821a6ec44ee831a0ddb5f
smithnj13 <SmithNJ13@hotmail.com>
2023-09-25 13:10:04 +0100

    Dynamic endpoint for seasonalPage + minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import React, {useState} from "react";
+import {Routes, Route, Link, useLocation} from "react-router-dom"
+import {SeasonalPage} from "./Pages/export"
 import './App.css'
 
 function App() {
-
+  const location = useLocation()
   return (
     <>
-      <h1>WEBSITE</h1>
+    {/* {location.pathname!=="/" && 
+      <div>NavBar</div>
+    } */}
+    <Routes>
+      <Route path="/" element={<div>
+        <Link to="/spring">Spring</Link>
+        <Link to="/summer">Summer</Link>
+        <Link to="/autumn">Autumn</Link>
+        <Link to="/winter">Winter</Link>
+      </div>} />
+        <Route path="/:season" element={<SeasonalPage />}>
+      </Route>
+    </Routes>
     </>
   )
 }
