a3c46f2f3e9851a518b92438a52e328caae763e4
filomena <filomena.francisco@outlook.com>
2023-09-25 10:59:49 +0100

    404 error message and adding links to Navbar, adding footer

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,20 @@
 
+import { NavLink, Outlet } from 'react-router-dom'
+import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <nav className='navbar'>
+    <div>
+     <NavLink to='/' className='navlink'>Home</NavLink> 
+     <NavLink to='/profile' className='navlink'>My Profile</NavLink>
+     <NavLink to='/addrecipe' className='navlink'>Add Recipe</NavLink>
+    </div>
+  </nav>    
+  <Outlet />
+  <footer className='footer'>SzndChef 2023</footer>
+    </>
   )
 }
 
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,19 @@
 
+import {Link} from 'react-router-dom'
+import "./NotFound.css"
+
 const NotFound = () => {
   return (
     <>
-    <h1>NotFound baybay </h1>
+    <h1>
+      404 - Page Not Found: Looks like we&apos;re out of season! 
+      </h1>
+    <h2>
+      Oops! The page you&apos;re looking for has gone on its own flavorful adventure in our seasonal orchard. Don&apos;t worry; it&apos;ll be back in season soon to delight your taste buds!
+      </h2>
+    <Link to="/" className="home_link"> Back to Home &rarr;</Link>
     </>
   )
 }
 
-export default NotFound
+export default NotFound;
