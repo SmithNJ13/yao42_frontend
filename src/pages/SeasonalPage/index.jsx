a25794c58480f89c9a517423b970cd6de6a13277
smithnj13 <SmithNJ13@hotmail.com>
2023-09-26 11:55:31 +0100

    reading info from backend, formatted recipe cards

--- src/pages/SeasonalPage/index.jsx
+++ src/pages/SeasonalPage/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import {CarouselComponent, RecipeCard} from "../../components"
+import axios from "axios"
 
 import "./style.css"
-
+// Declaring seasons for useParams comparison
 const seasons = ["spring", "summer", "autumn", "winter"]
 
 const SeasonalPage = () => {
@@ -11,6 +12,27 @@ const SeasonalPage = () => {
   if(!seasons.includes(season)) {
     return <Navigate replace={true} to="/notfound" />
   }
+  const [array,setArray] = useState(undefined)
+
+  // Fetching recipes from backend
+  async function getRecipes() {
+    axios.get("https://lap-4-server.onrender.com/recipes")
+    .then(resp => {
+      const data = resp.data.recipes
+      setArray(data)
+    })
+  }
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+  // Loading logic 
+  if(!array) {
+    return <div>Loading page!</div>
+  } else {
+    console.log(array)
+  }
+
 
   return (
     <>
@@ -27,6 +49,8 @@ const SeasonalPage = () => {
       </div>
     </div>
     <div id="RecipeInfo">
+      <RecipeCard />
+      <RecipeCard />
       <RecipeCard />
     </div>
     </>
