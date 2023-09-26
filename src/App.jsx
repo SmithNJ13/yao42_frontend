4fb8096ac6448658e772ba1aafa088689d3f8141
salinathapa <salina.business@hotmail.com>
2023-09-26 14:44:59 +0100

    create MixingBowl page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Routes, Route, Link, useLocation} from "react-router-dom"
 import {NavBar} from './components'
-import { NotFound, RegisterPage, Profile, SeasonalPage } from "./pages" 
+import { NotFound, RegisterPage, Profile, SeasonalPage, MixingBowl } from "./pages" 
 import './App.css'
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
     }
     <Routes>
       <Route path="/notfound" element={<NotFound />}></Route>
+      <Route path="/mixingbowl" element={<MixingBowl />}></Route>
       <Route path="/" element={<div>
         <Link to="/spring">Spring</Link>
         <Link to="/summer">Summer</Link>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,3 +2,4 @@ export { default as NotFound } from "./NotFound/index.jsx";
 export { default as Profile } from "./Profile/index.jsx"
 export { default as RegisterPage } from "./Register/index.jsx"
 export {default as SeasonalPage} from "./SeasonalPage"
+export {default as MixingBowl} from "./MixingBowl"
