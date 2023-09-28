436a8c07af97eca2e4f2a03d14ede1f05a62efd3
marcus-am <marcus_mm@outlook.com>
2023-09-28 16:13:04 +0100

    landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
+import LandingPage from "./LandingPage/index.jsx";
+
 export { default as NotFound } from "./NotFound/index.jsx";
 export { default as Profile } from "./Profile/index.jsx"
 export { default as RegisterPage } from "./Register/index.jsx"
 export { default as LoginPage } from "./Login/index.jsx"
 export { default as SeasonalPage } from "./SeasonalPage"
 export { default as MixingBowl } from "./MixingBowl"
+export { default  as LandingPage } from "./LandingPage"
