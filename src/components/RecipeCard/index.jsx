7d0640deb86cb5331755457daf80081442205290
smithnj13 <SmithNJ13@hotmail.com>
2023-09-29 10:05:46 +0100

    small styling

--- src/components/RecipeCard/index.jsx
+++ src/components/RecipeCard/index.jsx
@@ -3,20 +3,34 @@ import React from 'react'
 import Button from "react-bootstrap/Button"
 import Card from "react-bootstrap/Card"
 import "./style.css"
-import ViewRecipe from '../../pages/ViewRecipe'
 import { useNavigate } from 'react-router-dom'
-import {Routes, Route} from "react-router-dom"
 
 const RecipeCard = ({recipe, season}) => {
   const navigate = useNavigate()
   const name = recipe.name
+  let cardColour = "";
+
   function handleClick() {
     navigate(`/recipe/${name}`)
   }
+      switch(season) {
+      case "spring":
+        cardColour = "#edf6e0"
+        break;
+      case "summer":
+        cardColour = "#fff8d2"
+        break;
+      case "autumn":
+        cardColour = "#ffe8c0"
+        break;
+      case "winter":
+        cardColour = "#e1f2fa"
+        break;
+    }
 
   return (
     <>
-    <Card style={{ width: '20rem' }} className={season}>
+    <Card style={{ width: '20rem', backgroundColor: cardColour}} className={season}>
       <Card.Img variant="top" src={recipe.image} />
         <Card.Body>
           <Card.Title>{recipe.name}</Card.Title>
--- src/pages/ViewRecipe/index.jsx
+++ src/pages/ViewRecipe/index.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux"
+import { changeBGColour } from '../../actions/bgActions'
 import axios from 'axios'
 import "./style.css"
 
 const ViewRecipe = () => {
   const { name } = useParams()
   const [recipes, setRecipes] = useState()
+  const dispatch = useDispatch()
+  const BGColour = useSelector((state) => state.BGColour)
+  const BGStyle = {
+    backgroundColor: BGColour,
+  }
+
+  const handleBG = (colour) => {
+    dispatch(changeBGColour(colour))
+  }
 
   async function getRecipes() {
     await axios.get("https://lap-4-server.onrender.com/recipes")
@@ -16,6 +27,7 @@ const ViewRecipe = () => {
   }
   useEffect(() => {
     getRecipes()
+    handleBG("#F7F6FE")
   }, [])
   if(!recipes) {
     return <div className="loading">LOADING!</div>
@@ -23,23 +35,25 @@ const ViewRecipe = () => {
 
   return (
     <>
-    <div className="Sidebanner"/>
-    {recipes.filter((r) => r.name.includes(name))
-    .map((recipe, key) => (
-      <div id="Information" key={key}>
-        <div id="ImageBox">
-          <img src={recipe.image}/>
+    <body style={BGStyle}>
+      <div className="Sidebanner"/>
+      {recipes.filter((r) => r.name.includes(name))
+      .map((recipe, key) => (
+        <div id="Information" key={key}>
+          <div id="ImageBox">
+            <img src={recipe.image}/>
+          </div>
+          <div id="Heading">
+            <p>{name}</p>
+            <p>{recipe.season}</p>
+          </div>
+          <div id="MainBody">
+            <h1>{recipe.description}</h1>
+            <h1>{recipe.ingredients}</h1>
+          </div>
         </div>
-        <div id="Heading">
-          <p>{name}</p>
-          <p>{recipe.season}</p>
-        </div>
-        <div id="MainBody">
-          <h1>{recipe.description}</h1>
-          <h1>{recipe.ingredients}</h1>
-        </div>
-      </div>
-    ))}
+      ))}
+    </body>
     </>
   )
 }
