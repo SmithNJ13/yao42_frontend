1a0cba6369faef17b8a83288ef37e0e98a6af0ae
filomena <filomena.francisco@outlook.com>
2023-09-29 10:08:32 +0100

    adding lemon to not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,18 +1,20 @@
 
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLemon } from '@fortawesome/free-regular-svg-icons'
 import "./NotFound.css"
 
 const NotFound = () => {
   return (
     <>
-    <h1>
-      404 - Page Not Found: Looks like we&apos;re out of season! 
+    <div className='notfounddiv'>
+    <h1 className='notfoundh1'>
+      4<FontAwesomeIcon icon={faLemon} className='lemon'/>4 - Page Not Found: <br/> Looks like we&apos;re out of season! 
       </h1>
-    <h2>
-      Oops! The page you&apos;re looking for has gone on its own flavorful adventure in our seasonal orchard. Don&apos;t worry; it&apos;ll be back in season soon to delight your taste buds!
-      </h2>
+      <br/>
     <Link to="/" className="home_link"> Back to Home &rarr;</Link>
+    </div>
     </>
   )
 }
