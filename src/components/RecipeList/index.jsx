77a77596deeff39ebd3f871dc54a5bc23f1f3fee
salinathapa <salina.business@hotmail.com>
2023-09-29 11:30:37 +0100

    get recipes as recipe cards

--- src/components/RecipeList/index.jsx
+++ src/components/RecipeList/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
+import { RecipeCard } from "../../components"
+import './style.css'
 
-const RecipeList = ({ recipes }) => {
+const RecipeList = ({ recipes, season }) => {
   return (
-    <div>
+    <div className="recipe-cards">
       {recipes.length > 0 ? (
-        <div>
-          <ul>
-            {recipes.map((recipe, index) => (
-              <li key={index}>{recipe.name}</li>
-            ))}
-          </ul>
-        </div>
+        recipes.map((recipe, index) => (
+          <RecipeCard key={index} recipe={recipe} season={season} />
+        ))
       ) : (
         <p>No recipes found.</p>
       )}
