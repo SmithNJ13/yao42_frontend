3235b669d7c1d857a9a8f217637d1855195d19f9
salinathapa <salina.business@hotmail.com>
2023-10-02 12:48:07 +0100

    add swirly bg to navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,37 +1,56 @@
+import React from 'react';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import navbar from '../../assets/default-navbar.png';
 import './NavBar.css';
 
 const NavBar = () => {
   const location = useLocation();
 
-  let activeStyle = {
-    fontWeight: 'bold'};
-  
-  const getNavbarColor = () => {
-    switch (location.pathname) {
-      case '/spring':
-        return '#BADC83'; 
-      case '/summer':
-        return '#FFE448'; 
-      case '/autumn':
-        return '#FFA500';
-      case '/winter':
-        return '#87CEEB';
-      default:
-        return '#D296EE';
+  const getNavbarStyle = () => {
+    if (location.pathname === '/spring' || location.pathname === '/summer' || location.pathname === '/autumn' || location.pathname === '/winter') {
+      switch (location.pathname) {
+        case '/spring':
+          return {
+            backgroundColor: '#BADC83',
+            color: '#181A1B'
+          };
+        case '/summer':
+          return {
+            backgroundColor: '#FFE448',
+            color: '#181A1B'
+          };
+        case '/autumn':
+          return {
+            backgroundColor: '#FFA500',
+            color: '#181A1B'
+          };
+        case '/winter':
+          return {
+            backgroundColor: '#87CEEB',
+            color: '#181A1B'
+          };
+        default:
+          return {};
+      }
+    } else {
+      return {
+        backgroundImage: `url(${navbar})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      };
     }
   };
 
   return (
     <>
-      <div className="sidebar" style={{ backgroundColor: getNavbarColor() }}>
+      <div className="sidebar" style={getNavbarStyle()}>
         <nav className='navbar'>
           <div className='navlinks' data-testid="navlinks">
-            <NavLink to='/' className='navlink' style={({isActive}) => (isActive ? activeStyle : undefined)}>HOME</NavLink> 
-            <NavLink to='/profile' className='navlink' style={({isActive}) => (isActive ? activeStyle : undefined)}>MY PROFILE</NavLink>
-            <NavLink to='/recipe' className='navlink' style={({isActive}) => (isActive ? activeStyle : undefined)}>ADD RECIPE</NavLink>
-            <NavLink to='/mixingbowl' className='navlink' style={({isActive}) => (isActive ? activeStyle : undefined)}>MIXING BOWL</NavLink>
-            <NavLink to='/register' className='navlink' style={({isActive}) => (isActive ? activeStyle : undefined)}>SIGN UP</NavLink>
+            <NavLink to='/' className='navlink'>HOME</NavLink> 
+            <NavLink to='/profile' className='navlink'>MY PROFILE</NavLink>
+            <NavLink to='/recipe' className='navlink'>ADD RECIPE</NavLink>
+            <NavLink to='/mixingbowl' className='navlink'>MIXING BOWL</NavLink>
+            <NavLink to='/register' className='navlink'>SIGN UP</NavLink>
           </div>
         </nav>    
         <Outlet />
