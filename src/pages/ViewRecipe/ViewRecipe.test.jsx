792d6c9461816cd8a7fdf019f85649d0b2163eef
Balram Singh <balram_singh08@hotmail.com>
2023-10-03 17:02:12 +0100

    view recipe test half working

--- src/pages/ViewRecipe/ViewRecipe.test.jsx
+++ src/pages/ViewRecipe/ViewRecipe.test.jsx
@@ -1,68 +1,66 @@
 import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
-import { cleanup, render, screen } from '@testing-library/react';
-import { renderHook } from '@testing-library/react';
+import { cleanup, render, renderHook, screen, act } from '@testing-library/react';
+import React from 'react';
 import axios from 'axios';
-import userEvent from '@testing-library/user-event';
+import * as matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers)
 import ViewRecipe from '.';
-import { act } from 'react-dom/test-utils';
 import { Provider } from "react-redux"
 import store from '../../store';
 
 
-const mockAxios = vi.spyOn(require('axios'), 'get');
 
-const mockLocalStorage = {
-    getItem: vi.fn(),
-    setItem: vi.fn(),
-    removeItem: vi.fn(),    
-  };
+const mockRecipesData = 
+    [{
+      "id": 1,
+      "name": "Vegetable Pasta",
+      "description": "A light pasta dish with fresh spring vegetables.",
+      "ingredients": "200g pasta, 100g asparagus, 100g peas, 1 lemon, 50g parmesan",
+      "instructions": "",
+      "budget": "£",
+      "season": "Spring",
+      "image": "https://images.immediate.co.uk/production/volatile/sites/2/2021/06/OnePotPasta-47b5b0a-a302cf0.jpg?quality=90&resize=556,505",
+      "user_id": 1
+    }]
 
-const mockUseParams = vi.fn()
-const setUsername = vi.fn();
-const setEmail = vi.fn();
-
-
+const mockAxios = vi.spyOn(axios, 'get');
 
 describe('ViewRecipe component', () => {
-  beforeEach(() => {
-    global.localStorage = mockLocalStorage
-    
-    
-   localStorage.setItem('username', 'bob');
-   localStorage.setItem('email', 'bob@bob')
-    
-   
-    mockAxios.mockResolvedValue({ data: { recipes: [{
-        "id": 1,
-        "name": "Vegetable Pasta",
-        "description": "A light pasta dish with fresh spring vegetables.",
-        "ingredients": "200g pasta, 100g asparagus, 100g peas, 1 lemon, 50g parmesan",
-        "instructions": "",
-        "budget": "£",
-        "season": "Spring",
-        "image": "https://images.immediate.co.uk/production/volatile/sites/2/2021/06/OnePotPasta-47b5b0a-a302cf0.jpg?quality=90&resize=556,505",
-        "user_id": 1
-        }] } });
-    
-    
-    mockUseParams.mockReturnValue({ name: 'Vegetable Pasta' });
-  });
 
   afterEach(() => {
    cleanup();
   });
 
-  it('fetches recipe on correct authentication', () => {
-    setUsername.mockReturnValue(localStorage.getItem('username'));
-    setEmail.mockReturnValue(localStorage.getItem('email'));
-    
+  it('renders loading when recipes are not available',() => {
+    mockAxios.mockRejectedValueOnce({}); 
 
-        render(<Provider store={store}><ViewRecipe/></Provider>)
 
-    expect(setUsername).toHaveBeenCalledWith('bob');
-    expect(setEmail).toHaveBeenCalledWith('bob@bob');
+    render(
+    <Provider store={store}>
+        <ViewRecipe />
+    </Provider> 
+    )
 
-  })
-
+    const loadingElement = screen.getByText('loading...');
+    expect(loadingElement).toBeInTheDocument();
+  });
   
+  it('renders recipe details when recipes are available', async () => {
+    vi.spyOn(React, 'useState').mockImplementation(() => mockRecipesData);
+    mockAxios.mockResolvedValue({ data: { recipes: mockRecipesData } });
+    
+    render( <Provider store={store}>
+        <ViewRecipe />
+      </Provider> );
+
+    await act(async () => {
+      const recipeName = screen.getByText('Vegetable Pasta');
+      const recipeDescription = screen.getByText('A light pasta dish with fresh spring vegetables.');
+      const recipeIngredients = screen.getByText('200g pasta, 100g asparagus, 100g peas, 1 lemon, 50g parmesan');
+
+      expect(recipeName).toBeInTheDocument();
+      expect(recipeDescription).toBeInTheDocument();
+      expect(recipeIngredients).toBeInTheDocument();
+    });
+  });
 });
