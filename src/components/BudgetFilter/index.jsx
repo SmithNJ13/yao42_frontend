72a39576c9aee544ea91b0f6c795e996791716bf
salinathapa <salina.business@hotmail.com>
2023-10-04 10:17:40 +0100

    make budget filter into checkbox

--- src/components/BudgetFilter/index.jsx
+++ src/components/BudgetFilter/index.jsx
@@ -4,23 +4,33 @@ import { useNavigate, useLocation } from 'react-router-dom';
 const BudgetFilter = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const [selectedFilter, setSelectedFilter] = useState(null);
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
-    const filterParam = queryParams.get('filter');
-    setSelectedFilter(filterParam);
+    const filterParam = queryParams.getAll('filter');
+    setSelectedFilters(filterParam);
   }, [location.search]);
 
   const handleFilterChange = (filter) => {
-    setSelectedFilter(filter);
+    const updatedFilters = [...selectedFilters];
+    if (updatedFilters.includes(filter)) {
+      updatedFilters.splice(updatedFilters.indexOf(filter), 1);
+    } else {
+      updatedFilters.push(filter);
+    }
+    setSelectedFilters(updatedFilters);
+
     const queryParams = new URLSearchParams(location.search);
-    queryParams.set('filter', filter);
+    queryParams.delete('filter');
+    updatedFilters.forEach((f) => {
+      queryParams.append('filter', f);
+    });
     navigate(`${location.pathname}?${queryParams.toString()}`);
   };
 
   const clearFilter = () => {
-    setSelectedFilter(null);
+    setSelectedFilters([]);
     const queryParams = new URLSearchParams(location.search);
     queryParams.delete('filter');
     navigate(`${location.pathname}?${queryParams.toString()}`);
@@ -29,10 +39,34 @@ const BudgetFilter = () => {
   return (
     <>
       <h2> BUDGET FILTER:</h2>
-      {selectedFilter && <button onClick={clearFilter}>Clear Filter</button>}
-      <button className={`navlink ${selectedFilter === '£' ? 'selected' : ''}`} onClick={() => handleFilterChange('£')}>£</button>
-      <button className={`navlink ${selectedFilter === '££' ? 'selected' : ''}`} onClick={() => handleFilterChange('££')}>££</button>
-      <button className={`navlink ${selectedFilter === '£££' ? 'selected' : ''}`} onClick={() => handleFilterChange('£££')}>£££</button>
+      {selectedFilters.length > 0 && <button onClick={clearFilter}>Clear Filter</button>}
+      <label>
+        £
+        <input
+          type="checkbox"
+          value="£"
+          checked={selectedFilters.includes('£')}
+          onChange={() => handleFilterChange('£')}
+        />
+      </label>
+      <label>
+        ££
+        <input
+          type="checkbox"
+          value="££"
+          checked={selectedFilters.includes('££')}
+          onChange={() => handleFilterChange('££')}
+        />
+      </label>
+      <label>
+        £££
+        <input
+          type="checkbox"
+          value="£££"
+          checked={selectedFilters.includes('£££')}
+          onChange={() => handleFilterChange('£££')}
+        />
+      </label>
     </>
   );
 };
