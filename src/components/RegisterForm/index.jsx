632ace68063b6e5d0a4e4832825ba0b09c43c46a
marcus-am <marcus_mm@outlook.com>
2023-10-05 17:40:57 +0100

    last changes

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -64,17 +64,18 @@ const Register = () => {
   return (
     <div id='page' className='tw-flex tw-h-screen tw-justify-center tw-items-center tw-w-full'>
       
-      <div id='form-con' className='tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-ml-[250px] tw-shadow-lg tw-shadow-gray-600 tw-border-none'>
-      <div className='tw-hidden tw-relative md:tw-block tw-w-[700px] tw-h-[760px] tw-mx-auto leftsideform'>
-            <p className='tw-text-4xl tw-w-full tw-text-white tw-mt-10 tw-absolute tw-text-center tw-font-bold tw-text-7xl'>Welcome to </p> <img src={logo} className='logo' alt='szndChef logo'/>
+      <div id='form-con' className='tw-rounded-xl tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-ml-[250px] tw-shadow-lg tw-shadow-gray-600 tw-border-none'>
+      <div className='tw-hidden tw-rounded-l-xl tw-relative md:tw-block tw-w-[600px] tw-h-[760px] tw-mx-auto leftsideform'>
+            <p className='tw-text-4xl tw-w-full tw-text-white tw-mt-10 tw-absolute tw-text-center  tw-text-6xl'>Welcome to </p> 
+            <img src={logo} className='logo-register' alt='szndChef logo'/>
             <Lottie onComplete={() => {
-            }} lottieRef={cookingRef} animationData={animationData} className='lottie'/>
+            }} lottieRef={cookingRef} animationData={animationData} className='tw-object-cover tw-absolute tw-w-full tw-h-full tw-mt-[7rem]'/>
         </div>
-        <form style= {{ backgroundImage: `url(${form})`}} className= 'poppins-font tw-bg-cover tw-w-full tw-mx-auto tw-px-[4rem]' onSubmit={handleSubmit} autocomplete="off">
-        <h2 className='tw-text-left tw-text-4xl tw-text-white tw-font-bold tw-pt-[12.5rem] tw-pb-[4.5rem]  tw-text-5xl'>Register</h2>
-          <div className='tw-flex tw-flex-col tw-py-3 tw-text-xl'>
+        <form style= {{ backgroundImage: `url(${form})`}} className= 'poppins-font tw-rounded-r-xl tw-bg-cover tw-w-full tw-mx-auto tw-px-[4rem]' onSubmit={handleSubmit} autocomplete="off">
+        <h2 className='tw-text-center tw-text-4xl tw-text-white tw-font-bold tw-pt-[12.5rem] tw-pb-[2.5rem]  tw-text-[3rem]'>Register</h2>
+          <div className='tw-flex tw-flex-col tw-py-1.5 tw-text-lg'>
             <label htmlFor='username'>Username</label>
-            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.4rem] tw-text-sm' 
+            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.7rem] tw-text-base' 
               type='text' 
               placeholder='user123'
               value={name}
@@ -82,9 +83,9 @@ const Register = () => {
               id='username'
             />
           </div>
-          <div className='tw-flex tw-flex-col tw-py-3 tw-text-xl'>
+          <div className='tw-flex tw-flex-col tw-py-1.5 tw-text-lg'>
             <label htmlFor='email'>Email</label>
-            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.4rem] tw-text-sm' 
+            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.7rem] tw-text-base' 
               type='email' 
               placeholder='e.g. JohnDoe@email.com'
               value={email}
@@ -92,9 +93,9 @@ const Register = () => {
               id='email'
               />
           </div>
-          <div className='tw-flex tw-flex-col tw-py-3 tw-text-xl'>
+          <div className='tw-flex tw-flex-col tw-py-1.5 tw-text-lg'>
             <label htmlFor='password'>Password</label>
-            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.4rem] tw-text-sm' 
+            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.7rem] tw-text-base' 
               type='password' 
               placeholder='Please enter your password...'
               value={password}
@@ -102,9 +103,9 @@ const Register = () => {
               id='password'
               />
           </div>
-          <div className='tw-flex tw-flex-col tw-py-3 tw-text-xl'>
+          <div className='tw-flex tw-flex-col tw-py-1.5 tw-text-lg'>
             <label htmlFor='confirm'>Confirm Password</label>
-            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.4rem] tw-text-sm'
+            <input className='tw-border-2 tw-rounded-lg focus:tw-outline-none tw-border-gray-200 tw-p-[0.7rem] tw-text-base'
               type='password'  
               placeholder='Please re-enter your password...' 
               value={passConfirm}
@@ -112,8 +113,8 @@ const Register = () => {
               id='confirm'
               />
           </div>
-          <button type='submit' className='tw-border tw-rounded-2xl tw-flex tw-mx-auto tw-justify-center tw-transition tw-ease-in-out  tw-w-1/3 tw-my-2 tw-py-2 tw-text-white tw-font-bold formbutton'>Sign-Up</button>
-          <p className='tw-text-center tw-text-sm'>Already have an account? <Link to="/login" className='tw-font-bold tw-transition tw-ease-in-out formlink'>Login</Link> here !</p>
+          <button type='submit' className='tw-border tw-rounded-2xl tw-flex tw-mx-auto tw-justify-center tw-transition tw-ease-in-out tw-text-lg tw-w-1/3 tw-my-2 tw-py-2 tw-text-white tw-font-bold formbutton'>Sign-Up</button>
+          <p className='tw-text-center  tw-pt-1 tw-text-lg'>Already have an account? <Link to="/login" className='tw-font-bold tw-transition tw-ease-in-out formlink'>Login</Link> here !</p>
         </form>
       </div>
     </div>
